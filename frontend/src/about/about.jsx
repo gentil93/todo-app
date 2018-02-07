458877c739afd4078ee35880393aa6e20327d0e0
Gentil <matheusg93@gmail.com>
2018-02-07 18:26:48 -0200

    Primeiro commit frontend

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nos'></PageHeader>
+
+        <h2></h2>
+        <p></p>
+        <h2></h2>
+        <h2></h2>
+    </div>
+)
